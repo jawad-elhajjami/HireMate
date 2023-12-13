dc0af1ca35faf7f992fc349dcb70fbe8064c17c3
Ilyass Bezaiz <ibezaiz@gmail.com>
2023-12-13 21:22:39 +0100

    Creating the main Nav

---
+++ resources/views/components/employer-home/main-nav.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <nav class="slidemenu">
+        <!-- Item 1 -->
+        <input type="radio" name="slideItem" id="slide-item-1" class="slide-toggle" checked/>
+        <label for="slide-item-1">For you</label>
+        
+        <!-- Item 2 -->
+        <input type="radio" name="slideItem" id="slide-item-2" class="slide-toggle"/>
+        <label for="slide-item-2">Search</label>
+        
+        <!-- Item 3 -->
+        <input type="radio" name="slideItem" id="slide-item-3" class="slide-toggle"/>
+        <label for="slide-item-3">Activity</label>   
+
+        
+        <!-- Bar -->
+        <div class="slider">
+            <div class="bar"></div>
+        </div>
+    </nav>
+</div>
