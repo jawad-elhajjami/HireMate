047c289022ff54bdb96b15a054de4ca5fe0725ed
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2023-12-13 19:36:25 +0100

    Initial login form validation using livewire

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,4 +1,9 @@
-<label class="custom-field one mb-3">
-    <input type="{{ $inputType }}" placeholder=" "/>
+@props(['inputType', 'inputName'])
+
+@php
+    $errorClass = $errors->has($attributes['wire:model']) ? 'error' : '';
+@endphp
+<label class="custom-field one mb-3 {{ $errorClass }}">
+    <input type="{{ $inputType }}" placeholder=" " autocomplete="false" wire:model="{{ $attributes['wire:model'] }}"/>
     <span class="placeholder">{{$inputName}}</span>
 </label>
