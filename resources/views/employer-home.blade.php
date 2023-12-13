0c2a4fd897a3b99cbee024d98b6604e278362595
Ilyass Bezaiz <ibezaiz@gmail.com>
2023-12-13 20:37:13 +0100

    Creating the employer home page with the search section

---
+++ resources/views/employer-home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <title>{{ config('app.name') }} - Home page</title>
+</head>
+<body>
+    <x-employer-home.header showLinks="true"/>
+    <x-employer-home.search-section />
+    <x-footer />
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('Home');
 Route::view('/getStarted', 'getStarted')->name('GetStarted');
 Route::view('/login', 'login')->name('login');
+Route::view('/employer-home', 'employer-home')->name('employer-home');
